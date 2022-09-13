b0272caa4888631b3eaadca6c1c319a591b678fa
fvillaelis <106676052+fvillaelis@users.noreply.github.com>
2022-09-13 10:50:13 +0200

    Migrate to Java 17 , springboot 2.7.3 and spring security 5

--- src/main/java/com/flaminiovilla/security/config/WebMvcConfig.java
+++ src/main/java/com/flaminiovilla/security/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("*")
+        .allowedOrigins("http://localhost:3000", "http://localhost:8080")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true)
