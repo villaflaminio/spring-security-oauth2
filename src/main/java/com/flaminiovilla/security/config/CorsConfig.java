1a34b67a05e1e3cb142514c919184ebb71164507
villaflaminio <vifla01@gmail.com>
2022-10-03 17:50:55 +0200

    test cors disable

---
+++ src/main/java/com/flaminiovilla/security/config/CorsConfig.java
@@ -0,0 +1,47 @@
+package com.flaminiovilla.security.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Il Cross-Origin Resource Sharing (CORS) è un meccanismo che usa header HTTP addizionali
+ * per indicare che un dominio dispone dell'autorizzazione per accedere alle risorse selezionate
+ * **/
+@Configuration
+public class CorsConfig {
+
+   @Bean
+   public CorsFilter corsFilter() {
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowCredentials(false);
+      config.addAllowedOrigin("*"); // e.g. http://domain1.com
+      config.addAllowedMethod("*");
+      config.addAllowedHeader("*");
+
+      source.registerCorsConfiguration("/**", config);
+
+      return new CorsFilter(source);
+   }
+   @Bean
+   public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowCredentials(false);
+      config.setAllowedOrigins(Arrays.asList("http://localhost:8080", "*"));
+      config.setAllowedMethods(Collections.singletonList("*"));
+      config.setAllowedHeaders(Collections.singletonList("*"));
+      source.registerCorsConfiguration("/**", config);
+      FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+      bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+      return bean;
+   }
+}
