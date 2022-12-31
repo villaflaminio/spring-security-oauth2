ec3110f6e31bf9475fd7a83371e62cac5694843e
villaflaminio <vifla01@gmail.com>
2022-12-31 16:01:42 +0100

    use google as email client

--- src/main/java/com/flaminiovilla/security/util/MailSender.java
+++ src/main/java/com/flaminiovilla/security/util/MailSender.java
@@ -21,20 +21,17 @@ public class MailSender {
     private String password;
 
     @Bean
-    public JavaMailSender getOutlookMailServer(){
+    public JavaMailSender getGmailMailServer(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("elis-org.mail.protection.outlook.com");
-        mailSender.setPort(25);
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
 
         mailSender.setUsername(username);
         mailSender.setPassword(password);
-
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
+
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-
         return mailSender;
     }
 }
